2c99072d94eaecc157a1031d213dee05c6970979
MatiasSilva <mati598.sife@gmail.com>
2023-07-19 21:49:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('dotenv').config();
+var pool = require('./models/db');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+/* CONSULTAS DESPUES DE ESTA LINEA */
+
+/* SELECT */
+/* pool.query('select * from empleados').then(function(resultados){
+  console.log(resultados)
+}) */
+
+/* var obj = {
+  nombe : 'Matias',
+  apellido: 'Silva',
+  trabajo: 'docente',
+  edad:35,
+  salaio:1500,
+  mail: 'mati@gmail.com'
+}
+
+pool.query('insert into empleados set ?', [obj]).then(function(resultados){
+  console.log(resultados)
+}); */
+
+
+/* var id = 25;
+var obj = {
+  nomber: 'Marco',
+  apellido: 'Antonio'
+}
+
+pool.query('update empleados set ? wherer id_emp=?', [obj,id]).then(function(resultados){
+  console.log(resultados)
+}) */
+
+var id = 24;
+pool.query('delete from empleados wherrer id_emp=?',[id]).then(function(resultados){
+  console.log(resultados);
+})
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
